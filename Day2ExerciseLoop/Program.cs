5bf08ef882197293fad3ffaa2ca5832b7bb845d9
Matt B <mjbeezer11621@gmail.com>
2021-09-28 13:47:58 -0400

    First commit

---
+++ Day2ExerciseLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day2ExerciseLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Loop Exercise, display evens only back to user
+            
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i++);
+                }
+            }
+        }
+    }
+}
