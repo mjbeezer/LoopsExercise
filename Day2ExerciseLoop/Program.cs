9c8422b4652ddc4cac7dc3836e516be4dd73c75a
Matt B <mjbeezer11621@gmail.com>
2021-09-28 14:11:21 -0400

    second commit

--- Day2ExerciseLoop/Program.cs
+++ Day2ExerciseLoop/Program.cs
@@ -6,13 +6,30 @@ namespace Day2ExerciseLoop
     {
         static void Main(string[] args)
         {
-            //Loop Exercise, display evens only back to user
-            
+            //Loop Exercise, display even and odd numbers back to user. Even numbers first. Odd numbers second.
+            Console.WriteLine("Even Numbers");
             for (int i = 0; i <= 100; i++)
             {
                 if (i%2 == 0)
                 {
                     Console.WriteLine(i++);
+                }
+            }
+            
+            //While Loop Exercise
+            bool runProgram = true;
+            
+            while (runProgram)
+            {
+                Console.WriteLine("Do you want to continue looping? y/n");
+                string programCheck = Console.ReadLine();
+                if(programCheck == "y")
+                {
+                    runProgram = true;
+                }
+                else if(programCheck == "n")
+                {
+                    runProgram = false;
                 }
             }
         }
